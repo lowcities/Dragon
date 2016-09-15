fd15868e98d5c53b35f728277006164bf7fe9412
lowcities <musumashae@gmail.com>
2016-09-14 20:54:05 -0400

    Create slay.js
    I am still in my infancy with programming and coding. Hence the amateur level of program. I am keeping it aroung for a reference however.

---
+++ slay.js
@@ -0,0 +1,23 @@
+var slaying = true;
+var youHit = Math.floor(Math.random()*2);
+var damageThisRound = Math.floor(Math.random()*5 + 1);
+var totalDamage = 0;
+
+while(slaying){
+    if( youHit = 1 ){
+        console.log("ZipZapZow! You hit the Dragoon!");
+        totalDamage += damageThisRound;
+            if( totalDamage >=4){
+                console.log("Zippity ZowWOW! You have sleighed the Dragoon! You ar tha Mightiest Waryer of dem haalll!!");
+            slaying = false;    
+                }
+            else{
+              youHit = Math.floor(Math.random()*2);  
+                }
+        }
+    else{
+        console.log("Ahh Shiat! That there Dragoon Keeled yah!");
+          
+    slaying = false;
+    }
+}    
