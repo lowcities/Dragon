866ae9ec9e4df62298b152f156b9642d7b83db40
David Low <musumashae@gmail.com>
2017-05-03 20:23:22 -0400

    Update slay.js
    fixed the conditional operator on line 7

--- slay.js
+++ slay.js
@@ -3,8 +3,8 @@ var youHit = Math.floor(Math.random()*2);
 var damageThisRound = Math.floor(Math.random()*5 + 1);
 var totalDamage = 0;
 
-while(slaying){
-    if( youHit = 1 ){
+while(slaying) {
+    if( youHit == 1 ) {
         console.log("ZipZapZow! You hit the Dragoon!");
         totalDamage += damageThisRound;
             if( totalDamage >=4){
